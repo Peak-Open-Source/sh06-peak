11c1bc88458ae5ef5063938c098a7c72dd2921c7
2642865m <2642865m@student.gla.ac.uk>
2023-11-22 14:19:26 +0000

    Created model for structures in database

--- PSS_microservice/src/database.py
+++ PSS_microservice/src/database.py
@@ -7,4 +7,23 @@ protein_collection = db.get_collection("collectionTest")
 PyObjectId = Annotated[str, BeforeValidator(str)]
 
 class ProteinModel(BaseModel):
-    
+    id: Optional[PyObjectId] = Field(alias="_id", default=None)
+    sequence: str = Field(...) #get from fetch_pdb_by_id
+    pdb_name: str = Field(...) #same as above
+    url: str = Field(...)
+    model_config = ConfigDict(
+        populate_by_name=True,
+        arbitrary_types_allowed=True,
+        json_schema_extra={
+            "example":{
+                "sequence":"seqwa45",
+                "pdb_name":"pdb name woo",
+                "url":"www.google.com",
+            }
+        }
+    )
+
+class ProteinCollection(BaseModel):
+    proteins: List[ProteinModel]
+
+

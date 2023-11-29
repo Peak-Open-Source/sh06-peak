d8f7565b265d7aae298b152a88236a62b2502664
TaylorSim <taylorhsim@outlook.com>
2023-11-29 12:09:47 +0000

    didnt actually save whoops heres the actual change from pydantic to mongoengine

--- PSS_microservice/src/models.py
+++ PSS_microservice/src/models.py
@@ -1,22 +1,38 @@
 import uuid
 from typing import Optional
 from pydantic import BaseModel, Field
+from mongoengine import *
+connect('peak project') # come back to this
 
-class ProteinModel(BaseModel):
-    id: str = Field(default_factory=uuid.uuid4, alias="_id")
-    sequence: str = Field(...) #get from fetch_pdb_by_id
-    pdb_name: str = Field(...) #same as above
-    url: str = Field(...)
-    class Config:
-        populate_by_name=True,
-        arbitrary_types_allowed=True,
-        json_schema_extra={
-            "example":{
-                "sequence":"seqwa45",
-                "pdb_name":"pdb name woo",
-                "url":"www.google.com",
-            }
-        }
+# class User(Document):
+#     email = StringField(required=True)
+#     first_name = StringField(max_length=60)
+#     surname = StringField(max_length=69)
+
+class Protein(Document):
+    sequence = StringField() 
+    pdb_name = StringField()
+    url = StringField()
+    def __init__(self, s, pdb, url):
+        self.sequence=s, self.pdb_name=pdb, self.url=url
+    meta={'collection': 'protein_collection'}
+
+
+# class ProteinModel(BaseModel):
+#     id: str = Field(default_factory=uuid.uuid4, alias="_id")
+#     sequence: str = Field(...) #get from fetch_pdb_by_id
+#     pdb_name: str = Field(...) #same as above
+#     url: str = Field(...)
+#     class Config:
+#         populate_by_name=True,
+#         arbitrary_types_allowed=True,
+#         json_schema_extra={
+#             "example":{
+#                 "sequence":"seqwa45",
+#                 "pdb_name":"pdb name woo",
+#                 "url":"www.google.com",
+#             }
+#         }
     
 
 # class ProteinCollection(BaseModel):

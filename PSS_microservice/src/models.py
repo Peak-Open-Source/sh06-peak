27d026d4d89ef2250134e3c45c6c2782b8a45b9d
2642865m <2642865m@student.gla.ac.uk>
2023-11-29 14:35:20 +0000

    Template of method for writing to database

--- PSS_microservice/src/models.py
+++ PSS_microservice/src/models.py
@@ -1,43 +1,37 @@
-import pymongo
-from mongoengine import *
-# connect('peak project', host= 'localhost' port=27017) # come back to this
+from pymongo import MongoClient
 
-# class User(Document):
-#     email = StringField(required=True)
-#     first_name = StringField(max_length=60)
-#     surname = StringField(max_length=69)
+# MongoDB connection URI
+uri = 'mongodb+srv://proteinLovers:protein-Lovers2@cluster0.pbzu8xb.mongodb.net/?retryWrites=true&w=majority'
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["ProteinTest"]
-mycol = mydb["collectionTest"]
-class Protein(Document):
-    sequence = StringField() 
-    pdb_name = StringField()
-    url = StringField()
-    def __init__(self, s, pdb, url):
-        self.sequence=s, self.pdb_name=pdb, self.url=url
-    meta={'collection': 'protein_collection'}
+# Function to perform database operations
+def write_to_database():
+    try:
+        # Connect to the MongoDB cluster
+        client = MongoClient(uri)
+        
+        # Access the database and collection
+        database = client['ProteinTest']
+        collection = database['collectionTest']
 
-s1=Protein('sequence', 'pdb', 'url')
-s1.save()
-# class ProteinModel(BaseModel):
-#     id: str = Field(default_factory=uuid.uuid4, alias="_id")
-#     sequence: str = Field(...) #get from fetch_pdb_by_id
-#     pdb_name: str = Field(...) #same as above
-#     url: str = Field(...)
-#     class Config:
-#         populate_by_name=True,
-#         arbitrary_types_allowed=True,
-#         json_schema_extra={
-#             "example":{
-#                 "sequence":"seqwa45",
-#                 "pdb_name":"pdb name woo",
-#                 "url":"www.google.com",
-#             }
-#         }
-    
+        # Example document to insert
+        document = {
+            'name': 'Sample Protein',
+            'type': 'Example Type',
+            'description': 'This is a test protein document.'
+        }
 
-# class ProteinCollection(BaseModel):
-#     proteins: List[ProteinModel]
+        # Insert a single document
+        result = collection.insert_one(document)
+        
+        print(f"Inserted document ID: {result.inserted_id}")
+        print(f"Insertion successful!")
 
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
+    finally:
+        # Close the connection
+        client.close()
+
+# Call the function to perform database operations
+write_to_database()

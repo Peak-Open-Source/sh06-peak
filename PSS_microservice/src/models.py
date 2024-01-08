833c9ab5f16cdc6f84b954131b84d79061946b1a
2642865m <2642865m@student.gla.ac.uk>
2024-01-08 13:17:56 +0000

    Updated write_to_database method to write values passed from main.py to the database

--- PSS_microservice/src/models.py
+++ PSS_microservice/src/models.py
@@ -5,21 +5,22 @@ uri = 'mongodb+srv://proteinLovers:protein-Lovers2@cluster0.pbzu8xb.mongodb.net/
 
 # Function to perform database operations
 #Next step - take in variables passed from /PSS_microservice/main.py
-def write_to_database():
+def write_to_database(seq, path, url):
     try:
         # Connect to the MongoDB cluster
         client = MongoClient(uri)
         
         # Access the database and collection
-        database = client['ProteinTest']
-        collection = database['collectionTest']
+        database = client['ProteinDatabase']
+        collection = database['ProteinCollection']
 
-        # Example document to insert
-        # Next step - replace the current example strings with the variables passed in 
+        #Function is called in PSS_microservice/main.py - fetch_pdb_by_id
+        #passes the necessary values, SHOULD be written to teh database; need someone else to test, 
+        #then can  work on the delete/edit etc functions
         document = {
-            'Sequence': 'Sample Protein',
-            'PDB': 'Example pdb',
-            'URL': 'google.com'
+            'Sequence': seq,
+            'PDB': path,
+            'URL': url
         }
 
         # Insert a single document
@@ -36,7 +37,6 @@ def write_to_database():
         client.close()
 
 # Call the function to perform database operations
-# Below to be put in the main.py where appropriate
-write_to_database()
+#write_to_database()
 
 

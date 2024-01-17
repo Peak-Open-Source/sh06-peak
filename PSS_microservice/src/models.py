ef251d62cea8c35873b03911e978b0849531a082
2642865m <2642865m@student.gla.ac.uk>
2024-01-17 13:49:39 +0000

    Added 'update_file' function
    Added the function to update the PDB file field in the database, still
    needs to be tested but have potential other approaches if current one
    isn't appropriate

--- PSS_microservice/src/models.py
+++ PSS_microservice/src/models.py
@@ -15,8 +15,6 @@ def write_to_database(seq, path, url):
         collection = database['ProteinCollection']
 
         #Function is called in PSS_microservice/main.py - fetch_pdb_by_id
-#_id = ObjectID; just the id of each one, new compass version search by {_id: ObjectID('idnumber')} 
-        #old version (ours i think) {"_id":{"$oid":"object_id"}}
         #if already exists: update ; TO BE IMPLEMENTED
 
         document = {
@@ -25,8 +23,14 @@ def write_to_database(seq, path, url):
             'URL': url
         }
 
-        # Insert a single document
+        #exists = collection.find_one(document)
+        #if (exists == False):
+            # Insert a single document
         result = collection.insert_one(document)
+        #else:
+            #collection.update_one(exists, document)
+            #print("document exists")
+   
         
         print(f"Inserted document ID: {result.inserted_id}")
         print(f"Insertion successful!")
@@ -43,7 +47,8 @@ def write_to_database(seq, path, url):
 # Call the function to perform database operations
 
 
-
+#_id = ObjectID; just the id of each one, new compass version search by {_id: ObjectID('idnumber')} 
+#old version (ours i think) {"_id":{"$oid":"object_id"}}
 #sequence or key or pdb id
 def find(to_find, field):
 
@@ -52,7 +57,7 @@ def find(to_find, field):
         database = client['ProteinDatabase']
         collection = database['ProteinCollection']
 
-#search by sequence and pdb, unsure about key yet
+#search by sequence, pdb and _id
         if (field == "Sequence"):
             protein_info = {"Sequence":to_find}
         elif (field == "PDB"):
@@ -72,14 +77,33 @@ def find(to_find, field):
         client.close()
 
 
+#BElow; need to test, if not successful can copy doc, delete it and write_to_database with the old vals n new PDB file
+def update_file(id, new_file):
+    try:
+        client = MongoClient(uri)
+        database = client['ProteinDatabase']
+        collection = database['ProteinCollection']
 
+        protein_id = {"_id":{"$oid":id}}
 
+        protein = collection.find(protein_id)
+        protein = {"PDB":new_file}
 
+        
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    finally:
+        # Close the connection
+
+        client.close()
 
 
 
 
 
 #TO CONNECT - in mongoDB compass at connect paste in uri at top of file :)
+#& C:/Users/amypi/anaconda3/python.exe "c:/Users/amypi/OneDrive - University of Glasgow/PROJECT/PROJECT/sh06-main/PSS_microservice/main.py"
+#python sh06-main/cli/__main__.py get uniprot P12319   
 
 

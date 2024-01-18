1fadec50f6381160d1a479ab13dc098060417274
Drew Edgar <2647430E@student.gla.ac.uk>
2024-01-18 18:12:51 +0000

    updated pipeline to use new structure

--- tests/context.py
+++ tests/context.py
@@ -1,3 +1,5 @@
+# Adds needed modules and directories to current path for pytest tests
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../PSS_microservice')))

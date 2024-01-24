c1e116345b61ea59690673b49629a162265ee206
Anya Chaudhary <2649173c@student.gla.ac.uk>
2024-01-24 13:05:31 +0000

    Celery queue skeleton
    Added a task endpoint to check task status and created the predict endpoint with the celery decorator

---
+++ PSP_microservice/celery_config.py
@@ -0,0 +1,3 @@
+#broker_url = 'pyamqp://anya:123@localhost:5672/your_vhost'
+broker_url ='pyamqp://guest@localhost:5672//'
+result_backend = 'rpc://'
--- PSP_microservice/main.py
+++ PSP_microservice/main.py
@@ -3,10 +3,16 @@ from fastapi.responses import JSONResponse
 import requests 
 import json
 from fastapi.responses import RedirectResponse
+from celery import Celery
+from celery.result import AsyncResult
+import os
 
 
 app = FastAPI()
 
+
+celery = Celery('tasks')
+celery.config_from_object('celery_config')
 
 """
 App to retrive and request predictions from alphafold 
@@ -17,9 +23,13 @@ App to retrive and request predictions from alphafold
 def run_check():
     return {"message": "running! :)"}
 
+@celery.task
 def predict_protein_structure(sequence):
     #TODO use alphafold to predict the protien and then return the sequence
-    return
+    sequence = "Dummy sequence"
+    predicted_structure = "Dummy sequence"
+
+    return {'sequence': sequence, 'structure': predicted_structure}
 
 @app.post("/predict")
 async def predict_endpoint(sequence: str): #use async so that we can handle multiple requests coming in
@@ -27,7 +37,27 @@ async def predict_endpoint(sequence: str): #use async so that we can handle mult
     #TODO queue the sequence 
     #predict_protein_structure(sequence)
     #TODO store the results
-    return
+    
+    #queue task
+    task = predict_protein_structure.apply_async(args=[sequence])
+    #store the task id
+    task_id = task.id
+
+    # Check if the task is in the queue
+    is_in_queue = AsyncResult(task_id).state == 'PENDING'
+
+    return {"task_id": task_id, "in_queue": is_in_queue}
+
+@app.get("/task/{task_id}")
+async def read_task(task_id: str):
+    result = AsyncResult(task_id)
+    if result.state == 'PENDING':
+        raise HTTPException(status_code=404, detail="Task still in queue")
+    if result.state == 'PREDICTING':
+        raise HTTPException(status_code=404, detail="Prediction in progress")
+    if result.state == 'SUCCESS':
+        return {"result": result.result}
+    return {"status": result.state}
 
 # Endpoint to search predictions already stored in alpha 
 @app.get('/get_predicted/{qualifier}')
@@ -38,7 +68,7 @@ def get_prediction(qualifier):
         alphafold_dict = json.loads(result.content) #loads in the raw json data
     return {"aphafold_raw_data": alphafold_dict} #displays data
 
-@app.get('/getSequence/{qualifier}') 
+@app.get('/get_sequence/{qualifier}') 
 def get_alphafold_sequence(qualifier):
     url = f"https://alphafold.ebi.ac.uk/api/prediction/{qualifier}"
     result = requests.get(url)  # Fetch corresponding JSON from alphafold API

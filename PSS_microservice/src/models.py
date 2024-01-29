f98be59b42616d753017ea969e24394d33d50336
2642865m <2642865m@student.gla.ac.uk>
2024-01-29 12:46:17 +0000

    Added 'delete_file' function
    Takes in a value and a field to search for the document to delete; will
    clarify if entire document to be sent for deleting or  just a value to
    search for (current solution). Want to edit to call the 'search'
    function instead of searching within the delete function, but
    experiencing connection issues when trying that so will push this now so
    there IS a delete function

--- PSS_microservice/src/models.py
+++ PSS_microservice/src/models.py
@@ -1,6 +1,7 @@
 from mongoengine import *
 from mongoengine import disconnect
 from pymongo import *
+
 #below; creates a new protein object
 class ProteinCollection(Document): 
     Sequence = StringField(require=True)
@@ -9,6 +10,8 @@ class ProteinCollection(Document):
     #may have to change to URLField
     #may have to change PDB to FileField; both currently StringField for my dud tests at the bottom
     #once checked PDB fetch failure can change and test with URL/PDBField
+
+
 def write_to_database(seq, pdb, url):
     try:
         connect('ProteinDatabase',host="mongodb+srv://proteinLovers:protein-Lovers2@cluster0.pbzu8xb.mongodb.net/?retryWrites=true&w=majority")
@@ -38,8 +41,9 @@ def write_to_database(seq, pdb, url):
         disconnect()
 
 
-
 def search(to_find, field):
+    #returns entire document; not just value searched for; can be changed
+    #document = ProteinCollection.objects.only('Sequence').get(Sequence=to_find) ;  for specific fields to be returned
     try:
         connect('ProteinDatabase',host="mongodb+srv://proteinLovers:protein-Lovers2@cluster0.pbzu8xb.mongodb.net/?retryWrites=true&w=majority")
         meta={'collection':'ProteinCollection'}
@@ -71,10 +75,31 @@ def update_file(id_to_find, new_file):
     finally:
         disconnect()
 
+
+def delete_file(to_delete, field):
+    try:
+        connect('ProteinDatabase',host="mongodb+srv://proteinLovers:protein-Lovers2@cluster0.pbzu8xb.mongodb.net/?retryWrites=true&w=majority")
+        meta={'collection':'ProteinCollection'}
+        #want to call 'search' to avoid repeataing, causes connect error; check
+        if field == "Sequence":
+            document = ProteinCollection.objects.get(Sequence=to_delete)
+        elif field == "PDB":
+            document = ProteinCollection.objects.get(PDB=to_delete)
+        elif field == "Key":
+            document = ProteinCollection.objects.get(id=to_delete)
+        document.delete()
         
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        disconnect()
+
+
+
+
 #my little weeny tests; all working as intended
         
-#write_to_database("accatgagatsgstaaga","16fa","wikipedia.com")
+#delete_file("pdbdoc","PDB")
 #write_to_database("accatgagatsgstaaga","clobbering","wikipedia.com")
 #update_file('65afbb69f6a68a6a4e715d57', "1F6B")
 #doc_to_find = search("17fa" ,"PDB")

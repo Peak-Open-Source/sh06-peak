45363b8dc2d89ff519509134f4c7360fc1502874
Ross Smith <2652064s@student.gla.ac.uk>
2024-02-20 12:35:58 +0000

    Updated models.py documentation A lot of lines were too long and required fixing. Additionally, provided more in depth documentation for the ProteinCollection class.
    [Issue #97]

--- PSS_microservice/src/models.py
+++ PSS_microservice/src/models.py
@@ -3,25 +3,49 @@ from mongoengine import connect, Document, StringField, disconnect
 HOST_URL = "mongodb+srv://proteinLovers:protein-Lovers2@cluster0.pbzu8xb.mongodb.net/?retryWrites=true&w=majority"  # noqa:E501
 
 
-# below; creates a new protein object
 class ProteinCollection(Document):
+
+    """
+    A class used to represent the ProteinCollection table in our database.
+    Inherits from the mongoengine 'Document' class in order to be
+    accessed and updated correctly corresponding to our database.
+
+    Attributes
+    ----------
+
+    Sequence : str
+        The corresponding protein structure sequence of an entry.
+    PDB : str
+        The Protein Data Bank ID of the protein entry.
+    URL : str
+        The local download URL to download the PDB file locally
+    FileContent : str
+        The complete contents of the PDB file, which can allows files
+        to be created by deriving from it.
+
+    """
+
     Sequence = StringField(require=True)
     PDB = StringField(required=True)
     URL = StringField(required=True)
     FileContent = StringField(required=True)
 
 
-def create_or_update(seq, pdb, url, file_content):
+def create_or_update(seq: str, pdb: str, url: str, file_content: str) -> None:
     """
-    The function `create_or_update` connects to a MongoDB database, checks if a collection with a given
-    PDB exists, and either updates the existing entries or writes new entries to the database.
-    
-    :param seq: The `seq` parameter is a string representing the sequence of a protein. It could be a
-    sequence of amino acids or nucleotides, depending on the context
-    :param pdb: The parameter "pdb" is a string that represents the Protein Data Bank (PDB) code for a
-    protein. The PDB code is a unique identifier for a protein structure in the Protein Data Bank
-    database
-    :param url: The `url` parameter is a string that represents the URL of the protein sequence
+    The function `create_or_update` connects to a MongoDB database, checks if a
+    collection with a given PDB exists, and either updates the existing
+    entries or writes new entries to the database.
+
+    :param seq: The `seq` parameter is a string representing the sequence of a
+                protein. It could be asequence of amino acids or nucleotides,
+                depending on the context
+    :param pdb: The parameter "pdb" is a string that represents the Protein
+                Data Bank (PDB) code for a protein. The PDB code is a unique
+                identifier for a protein structure in the Protein Data Bank
+                database
+    :param url: The `url` parameter is a string that represents the URL of the
+                protein sequence
     """
 
     connect('ProteinDatabase', host=HOST_URL)
@@ -37,19 +61,23 @@ def create_or_update(seq, pdb, url, file_content):
         write_to_database(seq, pdb, url, file_content)
 
 
-def write_to_database(seq, pdb, url, file_content):
+def write_to_database(seq: str, pdb: str, url: str, file_content: str) -> None:
     """
-    The function `write_to_database` writes protein sequence, PDB ID, and URL to a MongoDB database,
-    checking for existing entries and updating if necessary.
-    
-    :param seq: The `seq` parameter is a string representing the sequence of a protein
-    :param pdb: The parameter "pdb" in the function "write_to_database" refers to the Protein Data Bank
-    (PDB) identifier. The PDB is a database that provides information about the 3D structures of
-    proteins. The PDB identifier is a unique alphanumeric code assigned to each protein structure in the
-    database
-    :param url: The `url` parameter in the `write_to_database` function is a string that represents the
-    URL of the protein structure. It is used to store the URL in the database along with the sequence
-    and PDB code of the protein
+    The function `write_to_database` writes protein sequence, PDB ID, and URL
+    to a MongoDB database, checking for existing entries and updating if
+    necessary.
+
+    :param seq: The `seq` parameter is a string representing the sequence of a
+                protein
+    :param pdb: The parameter "pdb" in the function "write_to_database" refers
+                to the Protein Data Bank (PDB) identifier. The PDB is a
+                database that provides information about the 3D structures of
+                proteins. The PDB identifier is a unique alphanumeric code
+                assigned to each protein structure in the database
+    :param url: The `url` parameter in the `write_to_database` function is a
+                string that represents the URL of the protein structure. It is
+                used to store the URL in the database along with the sequence
+                and PDB code of the protein
     """
 
     try:
@@ -80,17 +108,20 @@ def write_to_database(seq, pdb, url, file_content):
         disconnect()
 
 
-def search(to_find, field):
+def search(to_find: str, field: str) -> ProteinCollection:
     """
-    The function `search` connects to a MongoDB database, searches for a document based on the given
-    field and value, and returns the document if found.
-    
-    :param to_find: The `to_find` parameter is the value that you want to search for in the database. It
-    can be a sequence, PDB code, or a key/id of a document in the ProteinCollection
-    :param field: The "field" parameter is used to specify the field in the database that you want to
-    search for. It can have three possible values: "Sequence", "PDB", or "Key"
-    :return: the document that matches the search criteria specified by the "to_find" and "field"
-    parameters.
+    The function `search` connects to a MongoDB database, searches for a
+    document based on the given field and value, and returns the document if
+    found.
+
+    :param to_find: The `to_find` parameter is the value that you want to
+                    search for in the database. It can be a sequence, PDB code,
+                    or a key/id of a document in the ProteinCollection
+    :param field: The "field" parameter is used to specify the field in the
+                  database that you want to search for. It can have three
+                  possible values: "Sequence", "PDB", or "Key"
+    :return: the document that matches the search criteria specified by the
+             "to_find" and "field" parameters.
     """
 
     try:
@@ -112,22 +143,23 @@ def search(to_find, field):
         disconnect()
 
 
-def update_structure(id_to_find, new_structure):
+def update_structure(id_to_find: str, new_pdb: str) -> None:
     """
-    The function `update_structure` updates the PDB structure of a protein document in a MongoDB
-    database.
-    
-    :param id_to_find: The id of the protein structure you want to update
-    :param new_structure: The parameter "new_structure" is the updated structure that you want to assign
-    to the ProteinCollection document with the specified "id_to_find". It could be a string representing
-    the new structure or any other data type that you want to store as the new value for the "PDB" field
-    in the
+    The function `update_structure` updates the PDB structure of a protein
+    document in a MongoDB database.
+
+    :param id_to_find: The primary key ID of the protein structure you want to
+                       update
+    :param new_pdb: The parameter "new_pdb" is the updated
+                    PDB ID that you want to assign to the
+                    ProteinCollection document with the specified
+                    "id_to_find".
     """
 
     try:
         connect('ProteinDatabase', host=HOST_URL)
         document = ProteinCollection.objects.get(id=id_to_find)
-        document.PDB = new_structure
+        document.PDB = new_pdb
         document.save()
     except Exception as e:
         print(f"An error occurred: {e}")
@@ -135,16 +167,19 @@ def update_structure(id_to_find, new_structure):
         disconnect()
 
 
-def delete_file(to_delete, field):
+def delete_file(to_delete: str, field: str) -> None:
     """
-    The function `delete_file` deletes a document from a MongoDB collection based on a specified field
-    and value.
-    
-    :param to_delete: The `to_delete` parameter is the value that you want to use to identify the
-    document that you want to delete from the database. It can be the value of the `Sequence`, `PDB`, or
-    `Key` field, depending on the value of the `field` parameter
-    :param field: The "field" parameter is used to specify the field based on which the document should
-    be deleted. It can have three possible values: "Sequence", "PDB", or "Key"
+    The function `delete_file` deletes a document from a MongoDB collection
+    based on a specified field and value.
+
+    :param to_delete: The `to_delete` parameter is the value that you want to
+                      use to identify the document that you want to delete
+                      from the database. It can be the value of the `Sequence`,
+                      `PDB`, or `Key` field, depending on the value of the
+                      `field` parameter
+    :param field: The "field" parameter is used to specify the field based on
+                  which the document should be deleted. It can have three
+                  possible values: "Sequence", "PDB", or "Key"
     """
 
     try:

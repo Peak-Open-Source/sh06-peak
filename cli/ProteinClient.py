0700179ba060835097ca71965961043159250489
Ross Smith <2652064s@student.gla.ac.uk>
2024-02-21 19:07:01 +0000

    Added additional documentation docstrings to ProteinClient.py This includes all command functions. [Ticket #101]

--- cli/ProteinClient.py
+++ cli/ProteinClient.py
@@ -13,6 +13,8 @@ port = (input("Please enter the corresponding\
 while not port.isnumeric():
     port = input("Please enter the corresponding port \
                  for the PSS Microservice: ")
+
+# Create an assistant class to help with http requests
 client = PSSClient(ip, int(port))
 running = True
 
@@ -66,6 +68,16 @@ class Command():
 
 
 def get_by_key(key: str):
+    """
+    Calls the retrieve by key endpoint, and if it receives results,
+    downloads the PDB to the user's current working directory.
+
+    Parameters
+    ----------
+
+    key : str
+        The key to search the database for.
+    """
     protein_info, success = client.get("retrieve_by_key", [key])
     if not success or protein_info["status"] == 404:
         print("Retrieve by key failed - are you sure the key is valid?")
@@ -81,6 +93,16 @@ def get_by_key(key: str):
 
 
 def get_by_sequence(seq: str):
+    """
+    Calls the retrieve by sequence endpoint, and if it receives results,
+    downloads the PDB to the user's current working directory.
+
+    Parameters
+    ----------
+
+    seq : str
+        The sequence to search the database for.
+    """
     protein_info, success = client.get("retrieve_by_sequence", [seq])
     if not success or protein_info["status"] == 404:
         print("Retrieve by sequence failed - are you sure the sequence is valid?")  # noqa: E501
@@ -96,6 +118,17 @@ def get_by_sequence(seq: str):
 
 
 def get_best_uniprot(id: str):
+    """
+    Calls the retrieve by uniprot id, which finds the best associated
+    PDB file. If a valid PDB exist, it downloads it on the server, and
+    then downloads it locally to the user.
+
+    Parameters
+    ----------
+
+    id : str
+        The Uniprot ID to search Uniprot for, finding the best PDB.
+    """
     best_uniprot, success = client.get("retrieve_by_uniprot_id", [id])
     if not success:
         print("Invalid Uniprot ID")
@@ -121,6 +154,9 @@ def get_best_uniprot(id: str):
 
 
 def get(type: str, id: str):
+    """
+    Calls the associated function for the selected command.
+    """
     if type == "uniprot":
         get_best_uniprot(id)
     elif type == "key":
@@ -130,14 +166,29 @@ def get(type: str, id: str):
 
 
 def store(file_path: str, pdb_id: str, sequence: str):
+    """
+    Loads the given file path if possible, then sends the corresponding
+    file content, PDB ID, and sequence to the server.
+
+    Parameters
+    ----------
+
+    file_path : str
+        The path of the file to load.
+    pdb_id : str
+        The ID of the uploaded PDB.
+    sequence : str
+        The corresponding sequence of the uploaded PDB.
+
+    """
     try:
         with open(file_path, "r") as pdb_file:
             byte_info = pdb_file.read()
-            result, success = client.post("store",
-                                          {
+            result, success = client.post("store", {
                                               "pdb_id": pdb_id,
                                               "sequence": sequence,
-                                              "file_content": byte_info})
+                                              "file_content": byte_info}
+                                          )
             if "success" in result and result["success"]:
                 print("Upload successful!")
             else:
@@ -171,7 +222,9 @@ COMMANDS = {
             \"get [database_type] [id]\"\
             \nFetches and downloads best PDB from appropriate database.\
             \nValid database_type: uniprot, key, sequence\
-            \nExample Usage: get uniprot P12319",
+            \nExample Usage: get uniprot P12319\
+            \nExample Usage: get key a5b3c2\
+            \nExample Usage: get sequence abababababa",
         get,
         2
     ),

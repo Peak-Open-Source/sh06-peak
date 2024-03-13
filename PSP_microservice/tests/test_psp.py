7101ae7b2a77660c5f8b7b8afade9d5897646b96
Anya Chaudhary <2649173c@student.gla.ac.uk>
2024-03-13 16:30:29 +0000

    Closes #100 Testing queing tasks
    Added 3 new pytests to test queing prediction requests

--- PSP_microservice/tests/test_psp.py
+++ PSP_microservice/tests/test_psp.py
@@ -1,10 +1,19 @@
-from main import app, get_prediction, get_alphafold_sequence
+import pytest
+from main import app, get_prediction, get_alphafold_sequence, predict_endpoint, read_task
+from src.endpoints import predict_protein_structure, async_predict, get_task_status  # noqa: E501
 from fastapi.testclient import TestClient
 import sys
+
 sys.path.append("..")
 client = TestClient(app)
 
-test_taskID_1 = ""
+
+@pytest.fixture(scope='session')
+def celery_config():
+    return {
+        'broker_url': 'pyamqp://guest@localhost:5672//',
+        'result_backend': 'rpc://',
+    }
 
 
 def test_default_response():
@@ -65,3 +74,22 @@ def test_3D_model_redirect_P00722():
 def test_3D_model_redirect_():
     response = client.get('/showstruct/P00420')
     assert response.url == 'https://alphafold.ebi.ac.uk/entry/P00420'
+
+
+def test_predict_queue_task_1():
+    test_seq = "MTHLERSRILWFRDIVAEATYLGDHTMFLNTIILAFTISDGVYGSVFYAGTGLHFLHMVMLAAMLGVNYWRMRNYHLTAGHHVGYETTIIYTHVLDVIWLFLYVVFYWWGV"  # noqa: E501
+    result = predict_protein_structure.delay(test_seq)
+    assert result.status == "PENDING"
+
+
+def test_predict_queue_task_2():
+    sequence = "IGVSEATRIDPNAWVERWKAAGHYQAEAALLQCTADTLADAVLITTAHAWQHQGKTLFISRKTYRIDGSGQMAITVDVEVASDTPHPARIGLNCQLAQVAERVNWLGLGPQENYPDRLTAACFDRWDLPLSDMYTPYVFPSENGLRCGTRELNYGPHQWRGDFQFNISRYSQQQLMETSHRHLLHAEEGTWLNIDGFHMGIGGDDSWSPSVSAEFQLSAGRYHYQ"  # noqa: E501
+    result = predict_protein_structure.delay(sequence)
+    assert result.status == "PENDING"
+
+
+def test_get_status():
+    sequence = "QHQGKTLFISRKTYRIDGSGQMAITVDVEVASDTPHPARIGLNCQLAQVAERVNWLGLGPQENYPDRLTAACFDRWDLPLSDMYTPYVFPSENGLRCGTRELNYGPHQWRGDFQFNISRYSQQQLMETSHRHLLHAEEGTWLNIDGFHMGIGGDDSWSPSVSAEFQ"  # noqa: E501
+    result = async_predict(sequence)
+    assert result["in_queue"]
+
